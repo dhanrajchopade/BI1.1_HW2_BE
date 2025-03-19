9dca2a5097d611aa63e6cad601a2fc7035b36bf9
dhanrajchopade07 <118125243+dhanrajchopade@users.noreply.github.com>
2025-03-20 00:55:47 +0530

    Update hotel.models.js
    new

--- models/hotel.models.js
+++ models/hotel.models.js
@@ -1,84 +1,76 @@
+const mongoose = require("mongoose")
+const { stringify } = require("node:querystring")
+const { boolean } = require("webidl-conversions")
+
 const HotelSchema = new mongoose.Schema({
-  name: {
-    type: String,
+name:{
+    type:String,
     required: true
-  },
-  category: {
-    type: String,
-    enum: ['Budget', 'Mid-Range', 'Luxury', 'Boutique', 'Resort', 'Other'],
+},
+category:{type:String,
+    enum:['Budget', 'Mid-Range', 'Luxury', 'Boutique', 'Resort' , 'Other' ],
     required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  rating: {
-    type: Number,
-    min: 0,
-    max: 5,
-    default: 0
-  },
-  reviews: [{
-    type: String
-  }],
-  website: String,
-  phoneNumber: {
-    type: String,
-    required: true
-  },
-  checkInTime: {
-    type: String,
-    required: true
-  },
-  checkOutTime: {
-    type: String,
-    required: true
-  },
-  amenities: [{
-    type: String
-  }],
-  priceRange: {
-    type: String,
-    enum: ['$$ (11-30)', '$$$ (31-60)', '$$$$ (61+)', 'Other'],
-    required: true // If this field is mandatory
-  },
-  reservationsNeeded: {
-    type: Boolean,
-    default: false
-  },
-  isParkingAvailable: {
-    type: Boolean,
-    default: false
-  },
-  isWifiAvailable: {
-    type: Boolean,
-    default: false
-  },
-  isPoolAvailable: {
-    type: Boolean,
-    default: false
-  },
-  isSpaAvailable: {
-    type: Boolean,
-    default: false
-  },
-  isRestaurantAvailable: {
-    type: Boolean,
-    default: false
-  },
-  photos: [{
-    type: String
-  }]
-}, { timestamps: true });
+},
 
-// Custom validation for reviews and amenities if needed
-HotelSchema.path('reviews').validate(function(reviews) {
-  return reviews.length > 0; // Ensure at least one review
-}, 'At least one review is required.');
+location:{
+    type: String,
+    required: true
+},
+rating:{
+    type:Number,
+    min:0,
+    max:5,
+    default:0
+},
+reviews:[{type:String}],
+website:String,
+phoneNumber:{
+    type:String,
+    required: true
+},
+checkInTime:{
+    type:String,
+    required: true
+},
+checkOutTime :{
+    type: String,
+    required: true
+},
+amenities:[{
+type: String,
 
-HotelSchema.path('amenities').validate(function(amenities) {
-  return amenities.length > 0; // Ensure at least one amenity
-}, 'At least one amenity is required.');
+}],
 
-const Hotel = mongoose.model("Hotel", HotelSchema);
-module.exports = Hotel;
+priceRange:{
+    type: String,
+    enum:['$$ (11-30)', '$$$ (31-60)', '$$$$ (61+)' , 'Other']
+},
+reservationsNeeded:{
+    type: Boolean,
+    default: false
+},
+isParkingAvailable:{
+    type: Boolean,
+    default:false
+},
+isWifiAvailable:{
+    type:Boolean,
+    default:false
+},
+isPoolAvailable:{
+    type:Boolean,
+    default:false
+},
+isSpaAvailable:{
+    type: Boolean,
+    default: false
+},
+isRestaurantAvailable:{
+    type:Boolean,
+    default:false
+},
+photos:[{type:String}],
+},{timestamps:true})
+
+const Hotel = mongoose.model("Hotel", HotelSchema)
+module.exports= Hotel
